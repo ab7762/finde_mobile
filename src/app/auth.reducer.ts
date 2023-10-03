07ac1d43df2f22f2ad516819c89be2f389ed3e13
Mikko <ab7762@student.jamk.fi>
2023-10-03 11:04:25 +0300

    store_asennettu

---
+++ src/app/auth.reducer.ts
@@ -0,0 +1,16 @@
+import { createReducer, on } from "@ngrx/store";
+import * as AuthActions from "./auth.actions";
+
+export interface AuthState {
+  loggedIn: boolean;
+}
+
+export const initialState: AuthState = {
+  loggedIn: false,
+};
+
+export const authReducer = createReducer(
+  initialState,
+  on(AuthActions.login, (state) => ({ ...state, loggedIn: true })),
+  on(AuthActions.logout, (state) => ({ ...state, loggedIn: false }))
+);
--- src/app/login-forms/login-forms.component.ts
+++ src/app/login-forms/login-forms.component.ts
@@ -2,6 +2,10 @@ import { Component } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { AuthService } from "../auth.service";
 import { Router } from "@angular/router";
+import { Store } from "@ngrx/store";
+import { AuthState } from "../auth.reducer";
+import { login, logout } from "../auth.actions";
+import { Observable } from "rxjs";
 @Component({
   selector: "ns-login-forms",
   templateUrl: "./login-forms.component.html",
@@ -10,24 +14,26 @@ import { Router } from "@angular/router";
 export class LoginFormsComponent {
   myForm: FormGroup;
   data: any;
+  loggedIn$: Observable<boolean>;
 
   constructor(
     private authService: AuthService,
     private fb: FormBuilder,
-    private router: Router
+    private router: Router,
+    private store: Store<AuthState>
   ) {
     this.myForm = this.fb.group({
       email: ["", [Validators.required, Validators.email]],
       password: ["", [Validators.required]],
     });
+    this.loggedIn$ = this.store.select((state) => state.loggedIn);
   }
 
   submitLogin() {
     const email = this.myForm.get("email").value;
     const password = this.myForm.get("password").value;
     this.authService.logIn(email, password).subscribe((response) => {
-      this.data = response;
-      console.log(response);
+      this.store.dispatch(login());
     });
   }
 
